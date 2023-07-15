c00a15e7b88b305835ec84c0729a493464788c00
Harsh Karanwal <harshkaranwal2000@gmail.com>
2023-07-15 22:39:17 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -44,7 +44,7 @@ function App() {
           >
             BY HARSH KARANWAL
             <br></br>{" "}
-            <span style={{ marginLeft: "7rem" }}>& SHREYAS PATIL</span>
+            <span style={{ marginLeft: "7rem" }}>& KRITI JAIN</span>
           </p>
         </div>
       </div>
