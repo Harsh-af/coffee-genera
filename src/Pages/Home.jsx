8196eb9e186055c9e8e252d03fe955299002df2a
KritiJain1712 <kritijain1720@gmail.com>
2023-07-18 23:18:35 +0530

    added loader

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 import Body from "../Components/Body";
 import Appbar from "../Components/Appbar";
 import Tile1 from "../Components/Tile1";
+
 function Home() {
   useEffect(() => {
     // Scroll to the top when the component mounts
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div style={{ backgroundColor: "#bdb2a7" }}>
       <Appbar />
